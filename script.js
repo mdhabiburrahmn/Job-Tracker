24bd21c9e42135234acb456a64799d9320fe8c02
habibadnaan <hello.habibadnaan@gmail.com>
2026-09-01 06:20:49 +0600

    Some styles and logics are added.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 
 
 let currentTab= 'all';
+const tabActive = ['bg-[#3B82F6]', 'border-[#3B82F6]', 'text-white']
+const tabInActive = ['bg-transparent', 'text-slate-700', 'border-slate-300', 'text-black']
 
 
 function switchTab(tab){
     console.log(tab);
-    const tabs = document.
+    const tabs = [ 'all', 'interview', 'rejected'];
+    for( const t of tabs){
+        const tabName = document.getElementById('tab-'+ t);
+        if( t === tab){
+            tabName.classList.remove(...tabInActive);
+            tabName.classList.add(...tabActive);
+        }else{
+            tabName.classList.remove(...tabActive);
+            tabName.classList.add(...tabInActive);
+            
+        }
+    }
 }
